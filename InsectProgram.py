6ac31b722baf0354a9cbab3fe9f3a3d2fd5e420c
Chris <c_guerra@baylor.edu>
2022-02-09 13:18:28 -0600

    added gitignore file

--- InsectProgram.py
+++ InsectProgram.py
@@ -8,9 +8,10 @@ def main():
     print('The flight of this insect is: ', my_insect.get_flight())
 
     for count in range (10):
-        my_insect.flight()
+        my_insect.mile()
+        distance = str(my_insect.mile())
 
 
-    print("The insect flies: ", my_insect.get_flight())
+    print("The insect flies: ", distance,'miles')
 
 main()
