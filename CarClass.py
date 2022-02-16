feca8ddfb02fd1cbeaa0f19a8206c68911085571
Chris <c_guerra@baylor.edu>
2022-02-16 15:19:51 -0600

    final

--- CarClass.py
+++ CarClass.py
@@ -12,6 +12,11 @@ class Car:
     def brake(self):
         self.__speed -= 5
 
-    
     def get_speed(self):
         return self.__speed
+
+    def get_year_model(self):
+        return self.__year_model
+    
+    def get_make(self):
+        return self.__make
--- CarProgram.py
+++ CarProgram.py
@@ -2,10 +2,14 @@ import CarClass as c
 
 def main():
 
+    car = c.Car('2003 Chevy','Silverado')
+    print('Current Speed 0 is',car.get_speed(),'mph')
+    for i in range(0,5):
+        car.accelerate()
+        print('Current Speed',i+1,'is',car.get_speed(),'mph')
     
-
-
-
-
+    for i in range(5,10):
+        car.brake()
+        print('Current Speed',i+1,'is',car.get_speed(),'mph')
 
 main()
